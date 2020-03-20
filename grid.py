a6eab8fe8dc390aecc3f2e11a5c91e7c92fcf87b
md.bd <mohammad.buet@gmail.com>
2020-03-20 23:57:17 +0900

    first two examples

---
+++ grid.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+
+# create a label widget
+myLabel1 = Label(root, text='Hello World!')
+myLabel2 = Label(root, text='I am Mohammad Khan.')
+
+# shoving it onto the screen
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
+
+root.mainloop()
+
---
+++ hello.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+# create a label widget
+myLabel = Label(root, text='Hello World!')
+
+# shoving it onto the screen
+myLabel.pack()
+
+root.mainloop()
+
