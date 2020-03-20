8339f04a72b257085b6ee0fbea1a4b3b00781007
md.bd <mohammad.buet@gmail.com>
2020-03-21 01:27:47 +0900

    Disabled added

--- button.py
+++ button.py
@@ -5,7 +5,8 @@ root = Tk()
 
 
 
-myButton = Button(root, text='Click Me!')
+# myButton = Button(root, text='Click Me!')
+myButton = Button(root, text='Click Me!', state=DISABLED)
 myButton.pack()
 
 root.mainloop()
