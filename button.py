07c266873c83feed30695be227461714b7b000b5
md.bd <mohammad.buet@gmail.com>
2020-03-21 01:35:19 +0900

    adding command to the button

--- button.py
+++ button.py
@@ -3,11 +3,16 @@ from tkinter import *
 
 root = Tk()
 
-
+def myClick():
+    myLabel = Label(root, text='Look I clicked a Button!')
+    myLabel.pack()
 
 # myButton = Button(root, text='Click Me!')
 # myButton = Button(root, text='Click Me!', state=DISABLED)
-myButton = Button(root, text='Click Me!', padx=50, pady=50)
+# myButton = Button(root, text='Click Me!', padx=50, pady=50)
+# CAUTION: command=myClick() will not work!!!!
+myButton = Button(root, text='Click Me!', command=myClick, fg="green", bg='#000000')
+
 myButton.pack()
 
 root.mainloop()
