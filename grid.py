2e20aeaf51f912bf19aad483124836cacee1de00
md.bd <mohammad.buet@gmail.com>
2020-03-21 00:04:18 +0900

    grid examples column operations

--- grid.py
+++ grid.py
@@ -5,10 +5,15 @@ root = Tk()
 # create a label widget
 myLabel1 = Label(root, text='Hello World!')
 myLabel2 = Label(root, text='I am Mohammad Khan.')
+# this is another way of putting the label onto the screen
+myLabel3 = Label(root, text='           ').grid(row=1, column=1) 
 
 # shoving it onto the screen
 myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=0)
+myLabel2.grid(row=1, column=5)
+# myLabel2.grid(row=1, column=1)
+# myLabel2.grid(row=1, column=5)
+
 
 root.mainloop()
 
