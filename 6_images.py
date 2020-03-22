184904ae8dffb62b6d2a9f73da020b90b8624433
md.bd <mohammad.buet@gmail.com>
2020-03-23 02:03:44 +0900

    icons, images added

--- 6_images.py
+++ 6_images.py
@@ -1,5 +1,7 @@
 # Using Icons, Images, and Exit Buttons
 from tkinter import *
+from PIL import ImageTk, Image
+
 
 root = Tk()
 root.title("Image Displayer")
@@ -9,5 +11,10 @@ root.iconbitmap('./files/predator.ico')
 # quit button
 button_quit = Button(root, text="Exit Program", command=root.quit)
 button_quit.pack()
+
+my_img = ImageTk.PhotoImage(Image.open('./files/overwatch.jpg'))
+my_label = Label(image=my_img)
+my_label.pack()
+
 
 root.mainloop()
